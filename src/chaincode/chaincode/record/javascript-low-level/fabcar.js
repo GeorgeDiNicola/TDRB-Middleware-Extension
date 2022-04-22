cf4cd5ce6a407e62965e53c8ca512311b874f248
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2022-04-21 22:08:24 -0400

    added the chaincode and node code to the lib

---
+++ src/chaincode/chaincode/record/javascript-low-level/fabcar.js
@@ -0,0 +1,143 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+const shim = require('fabric-shim');
+const util = require('util');
+
+let Chaincode = class {
+
+  // The Init method is called when the Smart Contract 'fabcar' is instantiated by the blockchain network
+  // Best practice is to have any Ledger initialization in separate function -- see initLedger()
+  async Init(stub) {
+    console.info('=========== Instantiated record chaincode ===========');
+    return shim.success();
+  }
+
+  // The Invoke method is called as a result of an application request to run the Smart Contract
+  // 'fabcar'. The calling application program has also specified the particular smart contract
+  // function to be called, with arguments
+  async Invoke(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+
+    let method = this[ret.fcn];
+    if (!method) {
+      console.error('no function of name:' + ret.fcn + ' found');
+      throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+    }
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+
+  async queryRecord(stub, args) {
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting itemID hash value');
+    }
+    let recordId = args[0];
+
+    let recordAsBytes = await stub.getState(recordId); //get the car from chaincode state
+    if (!recordAsBytes || recordAsBytes.toString().length <= 0) {
+      throw new Error(recordId + ' does not exist: ');
+    }
+    console.log(recordAsBytes.toString());
+    return recordAsBytes;
+  }
+
+  async initLedger(stub, args) {
+    console.info('============= START : Initialize Ledger ===========');
+    let records = [];
+    records.push({
+      itemIdHash: 'test',
+      itemIdAES: '0',
+      itemHash: 'test',
+      ownedTableAES: 'test'
+    });
+
+    for (let i = 0; i < records.length; i++) {
+      records[i].docType = 'record';
+      await stub.putState(records[i].itemIdHash, Buffer.from(JSON.stringify(records[i])));
+      console.info('Added <--> ', records[i]);
+    }
+    console.info('============= END : Initialize Ledger ===========');
+  }
+
+  async createRecord(stub, args) {
+    console.info('============= START : Create Record ===========');
+    if (args.length != 4) {
+      throw new Error('Incorrect number of arguments. Expecting 4');
+    }
+
+    var record = {
+      docType: 'record',
+      itemIdHash: args[0], // shouldn't this be arg 0?
+      itemIdAES: args[1],
+      itemHash: args[2],
+      ownedTableAES: args[3]
+    };
+
+    await stub.putState(record.itemIdHash, Buffer.from(JSON.stringify(record)));
+    console.info('============= END : Create Record ===========');
+  }
+
+  /*
+  async queryAllCars(stub, args) {
+
+    let startKey = 'CAR0';
+    let endKey = 'CAR999';
+
+    let iterator = await stub.getStateByRange(startKey, endKey);
+
+    let allResults = [];
+    while (true) {
+      let res = await iterator.next();
+
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString('utf8'));
+
+        jsonRes.Key = res.value.key;
+        try {
+          jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+        } catch (err) {
+          console.log(err);
+          jsonRes.Record = res.value.value.toString('utf8');
+        }
+        allResults.push(jsonRes);
+      }
+      if (res.done) {
+        console.log('end of data');
+        await iterator.close();
+        console.info(allResults);
+        return Buffer.from(JSON.stringify(allResults));
+      }
+    }
+  }
+  */
+
+  async updateRecord(stub, args) {
+    console.info('============= START : updateRecord ===========');
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    let recordAsBytes = await stub.getState(args[0]);
+    let record = JSON.parse(recordAsBytes);
+    record.itemHash = args[1];
+
+    await stub.putState(args[0], Buffer.from(JSON.stringify(record)));
+    console.info('============= END : updateRecord ===========');
+  }
+
+};
+
+shim.start(new Chaincode());
+
---
+++ src/node_app_code/invoke.js
@@ -0,0 +1,64 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+
+async function main(itemIdHash, itemIdAES, itemHash, ownedTableAES) {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('user1');
+        if (!userExists) {
+            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('record');
+
+        // Submit the specified transaction.
+        await contract.submitTransaction('createRecord', itemIdHash, itemIdAES, itemHash, ownedTableAES);
+        //await contract.submitTransaction('createRecord', '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce', '3', 'c7ccb07fcc5eb9ab62751b873b1634967038510702e8c2ca15fa8cef6afdfb60', 'students');
+        
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+// process.argv
+const itemIdHash = process.argv[2];
+console.log(itemIdHash)
+const itemIdAES = process.argv[3];
+console.log(itemIdAES)
+const itemHash = process.argv[4];
+console.log(itemHash)
+const ownedTableAES = process.argv[5];
+console.log(ownedTableAES)
+
+main(itemIdHash, itemIdAES, itemHash, ownedTableAES);
+
