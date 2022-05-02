e3887a103f527cae05bc88c70995110ed811793f
GeorgeJDiNicola <george.j.dinicola@gmail.com>
2022-05-02 10:32:46 -0400

    final updates to the test scripts

--- src/test_delete.sh
+++ src/test_delete.sh
@@ -2,6 +2,6 @@
 for i in {40..1}
 do
         echo "Iteration: $i"
-        time python3 main.py --q "delete from student where student_id=${i}" --c "delete" --d ${i}
+        time python3 main.py --q "delete from student where id=${i}" --c "delete" --d ${i}
         sleep 5
 done 2>&1 | tee middleware_delete_test.log
--- src/test_update.sh
+++ src/test_update.sh
@@ -2,6 +2,6 @@
 for i in {1..40}
 do
         echo "Iteration: $i"
-        time python3 main.py --q "update student set name='updated' where student_id=${i}" --c "update" --u "${i},updated"
+        time python3 main.py --q "update student set name='updated' where id=${i}" --c "update" --u "${i},updated"
         sleep 5
 done 2>&1 | tee middleware_update_test.log
